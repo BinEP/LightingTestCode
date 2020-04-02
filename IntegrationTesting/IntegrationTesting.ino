11e5b012074a9d530894c0bd281d3f40427e738b
Brady Stoffel <stoff056@umn.edu>
2020-04-02 17:46:16 -0500

    First

---
+++ IntegrationTesting/IntegrationTesting.ino
@@ -0,0 +1,30 @@
+#include <WS2812FX.h>
+
+#define FASTLED_ESP32_I2S
+#include <FastLED.h>
+
+#define NUM_LEDS 10
+#define NUM_STRIPS 4
+
+
+CRGB leds[NUM_LEDS];
+//CLEDController *controllers[NUM_STRIPS];
+
+WS2812FX ws2812fx = WS2812FX(leds, NUM_LEDS);
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  ws2812fx.init();
+  
+  ws2812fx.setBrightness(100);
+  ws2812fx.setSpeed(200);
+  ws2812fx.setMode(FX_MODE_RAINBOW_CYCLE);
+  ws2812fx.start();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ws2812fx.service();
+}
